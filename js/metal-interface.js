3008768045585d052a184aed39277fc18bf5986f
Andrew Dalton and Dylan Dills <dylan@>
2017-06-29 09:58:18 -0700

    get a random band functionutilizing API key

--- js/metal-interface.js
+++ js/metal-interface.js
@@ -3,7 +3,11 @@ var Metal = require('./../js/metal.js').metalModule;
 var displayBand = function(band, bandName, bandGenre, bandCountry) {
   $('.showMetal').text("The band you were searching for was " + band + ". " + "Here's some info about them from the Encyclopedia Metallum: " + " Name: " + bandName + ", Genre: " + bandGenre + ", Country of Origin: " + bandCountry);
 };
-console.log(displayBand);
+
+var displayRandom = function(bandName, bandGenre, bandCountry) {
+  $('.randomMetal').text("The band you were searching for was " + band + ". " + "Here's some info about them from the Encyclopedia Metallum: " + " Name: " + bandName + ", Genre: " + bandGenre + ", Country of Origin: " + bandCountry);
+};
+// console.log(displayBand);
 
 $(document).ready(function() {
   var currentMetalObject = new Metal();
@@ -11,5 +15,10 @@ $(document).ready(function() {
     var band = $('#metal-band').val();
     $('#metal-band').val("");
     currentMetalObject.getMetal(band, displayBand);
+  });
+
+  var randomMetalObject = new Metal();
+  $('#random').click(function() {
+    randomMetalObject.getRandom(displayRandom);
   });
 });
--- js/metal.js
+++ js/metal.js
@@ -5,12 +5,22 @@ Metal = function(){
 
 Metal.prototype.getMetal = function(band, displayBand) {
   $.get('http://em.wemakesites.net/search/band_name/' + band + '?api_key=' + apiKey).then(function(response) {
-    console.log(JSON.parse(response));
+    // console.log(JSON.parse(response));
     var parsedResponse = JSON.parse(response);
-    console.log(parsedResponse.data.search_results[0]);
+    // console.log(parsedResponse.data.search_results[0]);
     displayBand(band, parsedResponse.data.search_results[0].name, parsedResponse.data.search_results[0].genre, parsedResponse.data.search_results[0].country);
   }).fail(function(error) {
     $('.showMetal').text(error.responseJSON.message);
+  });
+};
+
+Metal.prototype.getRandom = function(displayRandom) {
+  $.get('http://em.wemakesites.net/band/random/' +'?api_key=' + apiKey).then(function(response) {
+    var parsedResponse = JSON.parse(response);
+    console.log(parsedResponse);
+    displayRandom(parsedResponse.data.band_name, parsedResponse.data.details.genre, parsedResponse.data.details.country);
+  }).fail(function(error) {
+    $('.randomMetal').test(error.responseJSON.message);
   });
 };
 
